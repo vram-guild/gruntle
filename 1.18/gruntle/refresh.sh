87b1b34d34ce40ab07d051a3d82bd21e61c7368c
grondag <grondag@users.noreply.github.com>
2021-12-05 08:44:14 -0800

    Use common refresh code in 1.18

--- 1.18/gruntle/refresh.sh
+++ 1.18/gruntle/refresh.sh
@@ -1,7 +1,21 @@
+readonly MC_TAG="mc118"
+# https://www.curseforge.com/minecraft/mc-mods/modmenu/files
+readonly MOD_MENU_VERSION="3.0.0"
+# https://www.curseforge.com/minecraft/mc-mods/cloth-config/files
+readonly CLOTH_CONFIG_VERSION="6.0.45"
+# https://www.curseforge.com/minecraft/mc-mods/roughly-enough-items/files
+readonly REI_VERSION="7.0.343"
+
+# https://fabricmc.net/versions.html
+readonly LOADER_VERSION="0.12.8"
+readonly MC_FULL_VERSION="1.18"
+
+### START COMMON CODE ##########################################
+
 # updates version for dependencies on maven.vram.io
 # args
 # 1 - name of the dependency as it would appear in gradle, like io.vram:bitkit
-#     if it is version-specific, name should include those tags, like io.vram:frex-fabric-mc118
+#     if it is version-specific, name should include those tags, like io.vram:frex-fabric-mc117
 # 2 - path to target .gradle file, relative to project root folder
 #     should generally start with fabric/ forge/ or quilt/
 updateVersion()
@@ -25,7 +39,8 @@ updateVersion()
 # 2 - target version string, like 2.0.14
 # 3 - path to target .gradle file, relative to project root folder
 #     should generally start with fabric/ forge/ or quilt/
-updateStaticVersion() {
+updateStaticVersion()
+{
   if grep -q $1 $3; then
     if grep -q $1:$2 $3; then
       echo $1:$2 "is already current"
@@ -36,38 +51,47 @@ updateStaticVersion() {
   fi
 }
 
+publishFabric()
+{
+  cd fabric
+  ./gradlew publish --rerun-tasks
+  # Gradle/loom won't re-include nested jars without this
+  # Also won't do it with --rerun-tasks on githubRelease - has to be a build
+  ./gradlew --stop
+  ./gradlew build --rerun-tasks
+  ./gradlew githubRelease
+  cd ..
+}
+
 updateVersion io.vram:bitkit fabric/project.gradle
 updateVersion io.vram:bitraster fabric/project.gradle
 updateVersion io.vram:special-circumstances fabric/project.gradle
 
-updateVersion io.vram:frex-fabric-mc118 fabric/project.gradle
-updateVersion io.vram:jmx-fabric-mc118 fabric/project.gradle
-updateVersion io.vram:canvas-fabric-mc118 fabric/project.gradle
+updateVersion "io.vram:frex-fabric-$MC_TAG" fabric/project.gradle
+updateVersion "io.vram:jmx-fabric-$MC_TAG" fabric/project.gradle
+updateVersion "io.vram:canvas-fabric-$MC_TAG" fabric/project.gradle
 
-updateVersion grondag:exotic-art-core-mc118 fabric/project.gradle
-updateVersion grondag:exotic-art-tech-mc118 fabric/project.gradle
-updateVersion grondag:exotic-art-test-mc118 fabric/project.gradle
-updateVersion grondag:exotic-art-unstable-mc118 fabric/project.gradle
-updateVersion grondag:exotic-matter-mc118 fabric/project.gradle
-updateVersion grondag:fermion-gui-mc118 fabric/project.gradle
-updateVersion grondag:fermion-mc118 fabric/project.gradle
-updateVersion grondag:fermion-modkeys-mc118 fabric/project.gradle
-updateVersion grondag:fermion-orientation-mc118 fabric/project.gradle
-updateVersion grondag:fermion-simulator-mc118 fabric/project.gradle
-updateVersion grondag:fermion-varia-mc118 fabric/project.gradle
-updateVersion grondag:fluidity-mc118 fabric/project.gradle
-updateVersion grondag:fonthack-mc118 fabric/project.gradle
-updateVersion grondag:mcmarkdown-mc118 fabric/project.gradle
+updateVersion "grondag:exotic-art-core-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:exotic-art-tech-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:exotic-art-test-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:exotic-art-unstable-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:exotic-matter-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fermion-gui-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fermion-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fermion-modkeys-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fermion-orientation-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fermion-simulator-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fermion-varia-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fluidity-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:fonthack-$MC_TAG" fabric/project.gradle
+updateVersion "grondag:mcmarkdown-$MC_TAG" fabric/project.gradle
 
-# https://www.curseforge.com/minecraft/mc-mods/modmenu/files
-updateStaticVersion com.terraformersmc:modmenu 3.0.0 fabric/project.gradle
-# https://www.curseforge.com/minecraft/mc-mods/cloth-config/files
-updateStaticVersion me.shedaniel.cloth:cloth-config-fabric 6.0.45 fabric/project.gradle
-# https://www.curseforge.com/minecraft/mc-mods/roughly-enough-items/files
-updateStaticVersion me.shedaniel:RoughlyEnoughItems-fabric 7.0.343 fabric/project.gradle
+updateStaticVersion com.terraformersmc:modmenu $MOD_MENU_VERSION fabric/project.gradle
+updateStaticVersion me.shedaniel.cloth:cloth-config-fabric $CLOTH_CONFIG_VERSION fabric/project.gradle
+updateStaticVersion me.shedaniel:RoughlyEnoughItems-fabric $REI_VERSION fabric/project.gradle
 
-sed -i '' 's/"fabricloader": ".*"/"fabricloader": ">=0.12.8"/' fabric/src/main/resources/fabric.mod.json
-sed -i '' 's/"minecraft": ".*"/"minecraft": "1.18"/' fabric/src/main/resources/fabric.mod.json
+sed -i '' "s/\"fabricloader\": \".*\"/\"fabricloader\": \">=$LOADER_VERSION\"/" fabric/src/main/resources/fabric.mod.json
+sed -i '' "s/\"minecraft\": \".*\"/\"minecraft\": \"$MC_FULL_VERSION\"/" fabric/src/main/resources/fabric.mod.json
 
 if [[ $1 == 'auto' ]]; then
     if output=$(git status --porcelain) && [ -z "$output" ]; then
@@ -77,16 +101,13 @@ if [[ $1 == 'auto' ]]; then
       subUrl=${maven_group//[:\.]/\/}
       major_minor=$(grep "mod_version=" "fabric/gradle.properties" | sed -n 's:.*mod_version=\(.*\):\1:p')
       patch=$(git rev-list --count HEAD)
-      mavenVer=$(curl -s "https://maven.vram.io/$subUrl/$mod_name-fabric-mc118/maven-metadata.xml" | grep "<release>" | sed -n 's:.*<release>\(.*\)</release>.*:\1:p')
+      mavenVer=$(curl -s "https://maven.vram.io/$subUrl/$mod_name-fabric-$MC_TAG/maven-metadata.xml" | grep "<release>" | sed -n 's:.*<release>\(.*\)</release>.*:\1:p')
 
       if [[ "$mavenVer" == "$major_minor.$patch" ]]; then
         echo "No Gruntle update actions required - no dependency changes and latest publish version $major_minor.$patch is current with the commit log count."
       else
         echo "Publishing jar because maven release $mavenVer is not current with expected version $major_minor.$patch."
-        cd fabric
-        ./gradlew publish --rerun-tasks
-        ./gradlew githubRelease --rerun-tasks
-        cd ..
+        publishFabric
       fi
     else
       echo "Gruntle made changes. Attempting automatic check-in."
@@ -94,23 +115,17 @@ if [[ $1 == 'auto' ]]; then
       cd fabric
 
       if ./gradlew build; then
+        cd ..
         echo "Gradle build successful, commiting changes to git"
         git add *
         git commit -m "Gruntle automatic update"
         git push
 
-        echo "Publishing to maven"
-        cd fabric
-        ./gradlew publish --rerun-tasks
-        # Gradle/loom won't re-include nested jars without this
-        # Also won't do it with --rerun-tasks on githubRelease - has to be a build
-        ./gradlew build --rerun-tasks
-        ./gradlew githubRelease
-        cd ..
+        echo "Publishing"
+        publishFabric
       else
+        cd ..
         echo "Gradle build failed. Cannot continue."
       fi
-
-      cd ..
     fi
 fi
