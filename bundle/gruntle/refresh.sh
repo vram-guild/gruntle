8b706ee6dcba80dd09a0581c6292e82eca528743
grondag <grondag@users.noreply.github.com>
2022-09-09 07:57:04 -0700

    Updates for 1.19.2

--- bundle/gruntle/refresh.sh
+++ bundle/gruntle/refresh.sh
@@ -1,25 +1,25 @@
 readonly MC_TAG="mc119"
 # https://www.curseforge.com/minecraft/mc-mods/modmenu/files
-readonly MOD_MENU_VERSION="4.0.0"
+readonly MOD_MENU_VERSION="4.0.6"
 # https://www.curseforge.com/minecraft/mc-mods/cloth-config/files
-readonly CLOTH_CONFIG_VERSION="7.0.72"
+readonly CLOTH_CONFIG_VERSION="8.2.88"
 # https://www.curseforge.com/minecraft/mc-mods/cloth-config-forge/files
-readonly CLOTH_CONFIG_FORGE_VERSION="7.0.72"
+readonly CLOTH_CONFIG_FORGE_VERSION="8.2.88"
 # https://www.curseforge.com/minecraft/mc-mods/roughly-enough-items/files
-readonly REI_VERSION="9.1.500"
-readonly REI_FORGE_VERSION="9.1.500"
+readonly REI_VERSION="9.1.530"
+readonly REI_FORGE_VERSION="9.1.530"
 # https://www.curseforge.com/minecraft/mc-mods/architectury-api/files
 # https://modrinth.com/mod/architectury-api/versions
-readonly ARCH_VERSION="5.7.28"
-readonly ARCH_FORGE_VERSION="5.7.28"
+readonly ARCH_VERSION="6.2.43"
+readonly ARCH_FORGE_VERSION="6.2.43"
 # https://maven.gegy.dev/releases/dev/lambdaurora/spruceui
 readonly SPRUCE_UI_VERSION="4.0.0+1.19"
 
 # https://fabricmc.net/versions.html
-readonly LOADER_VERSION="0.14.8"
-readonly MC_FULL_VERSION="1.19"
+readonly LOADER_VERSION="0.14.9"
+readonly MC_FULL_VERSION="1.19.2"
 # Following is used in fabric.mod.json because pre-release suffixes are apparently parsed differently there
-readonly MC_SHORT_VERSION="1.19"
+readonly MC_SHORT_VERSION="1.19.2"
 
 ### START COMMON CODE ##########################################
 
