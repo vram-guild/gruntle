bf81a7789fefa05903ed342402f66aefb38f41e3
spiralhalo <re.nanashi95@gmail.com>
2023-05-03 23:47:28 +0700

    23w13a_or_b
    Forge isn't supported
    Spruce UI isn't available

--- bundle/gruntle/refresh.sh
+++ bundle/gruntle/refresh.sh
@@ -1,27 +1,27 @@
 # https://www.curseforge.com/minecraft/mc-mods/modmenu/files
-readonly MOD_MENU_VERSION="6.2.1"
+readonly MOD_MENU_VERSION="6.2.2"
 # https://www.curseforge.com/minecraft/mc-mods/cloth-config/files
-readonly CLOTH_CONFIG_VERSION="10.0.96"
+readonly CLOTH_CONFIG_VERSION="11.0.97"
 # https://www.curseforge.com/minecraft/mc-mods/cloth-config-forge/files
-readonly CLOTH_CONFIG_FORGE_VERSION="10.0.96"
+readonly CLOTH_CONFIG_FORGE_VERSION="11.0.97"
 # https://www.curseforge.com/minecraft/mc-mods/roughly-enough-items/files
-readonly REI_VERSION="11.0.597"
-readonly REI_FORGE_VERSION="11.0.597"
+readonly REI_VERSION="11.0.599"
+readonly REI_FORGE_VERSION="11.0.599"
 # https://www.curseforge.com/minecraft/mc-mods/architectury-api/files
 # https://modrinth.com/mod/architectury-api/versions
-readonly ARCH_VERSION="8.1.79"
-readonly ARCH_FORGE_VERSION="8.1.79"
+readonly ARCH_VERSION="9.0.2"
+readonly ARCH_FORGE_VERSION="9.0.2"
 # https://maven.gegy.dev/releases/dev/lambdaurora/spruceui
-readonly SPRUCE_UI_VERSION="4.0.0+1.19"
+readonly SPRUCE_UI_VERSION="4.2.0+1.19.4"
 
 # https://fabricmc.net/versions.html
 readonly LOADER_VERSION="0.14.19"
 # Used as base version number for all mods specific to MC version
 # Last digit will be git commit number
-readonly MOD_VERSION="19.4"
-readonly MC_FULL_VERSION="1.19.4"
+readonly MOD_VERSION="20.0"
+readonly MC_FULL_VERSION="23w13a_or_b"
 # Following is used in fabric.mod.json because pre-release suffixes are apparently parsed differently there
-readonly MC_SHORT_VERSION="1.19.4"
+readonly MC_SHORT_VERSION="23w13a_or_b"
 
 ### START COMMON CODE ##########################################
 
